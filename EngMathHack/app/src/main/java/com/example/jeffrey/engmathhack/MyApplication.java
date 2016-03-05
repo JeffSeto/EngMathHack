4dd3eeaf7730d315eca8b4ce833728631eb29c4f
JeffSeto <jeffreyseto1@gmail.com>
2016-03-04 20:18:11 -0500

    Initial Push
    Initail push with all required libraries/dependencies.

---
+++ EngMathHack/app/src/main/java/com/example/jeffrey/engmathhack/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.jeffrey.engmathhack;
+
+/**
+ * Created by Jeffrey on 2016-03-04.
+ */
+public class MyApplication extends android.app.Application {
+
+    public MyApplication(){
+        super();
+    }
+
+    public void onCreate() {
+
+    }
+}
